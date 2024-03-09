cb5d2babe60c10823047309df623b7f5047556c7
Shi Qiu <shi.qiu@envistaco.cn>
2024-03-09 16:05:55 +0800

    temp archive

--- measure.py
+++ measure.py
@@ -2,6 +2,7 @@ import queue
 
 import cv2
 import numpy as np
+from scipy.interpolate import make_interp_spline
 
 
 class Measure:
@@ -62,26 +63,45 @@ class Measure:
 
     def sperate_cap_by_curvature(self):
         img = cv2.cvtColor(self.src_img, cv2.COLOR_BGR2GRAY)
-        img = cv2.blur(img, (5, 5))
         contours, _ = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
         # 选择最大的轮廓
         contour = max(contours, key=cv2.contourArea)
-        # 采样
-        sample_size = 100
-        interval = max(1, len(contour) // sample_size)
-        contour = contour[::interval]
-        # 计算曲率，即角度变化除以相邻点之间的距离
-        curvatures = self.getCurvature(contour)
 
-        # for index in range(len(curvatures)):
-        #     if index%10 ==0:
-        #         text = "{:.2f}".format(curvatures[index]*100)
-        #         cv2.putText(src_img, text, (contour[index][0][0], contour[index][0][1]), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
-        #                 (255, 0, 0), 1)
-        # cv2.imshow('Image with split contours', src_img)
-        # cv2.waitKey(0)
-        # cv2.destroyAllWindows()
+        # 计算轮廓的最小外接矩形
+        x, y, w, h = cv2.boundingRect(contour)
+        mbr_rect = (x, y, x + w, y + h)
+
+        # 从原图像中裁剪出最小外接矩形区域
+        mbr_image = img[y:y + h, x:x + w]
+
+        # 对裁剪出的图像进行中值滤波
+        # 注意：中值滤波的ksize必须是大于1的奇数
+        blur_core_size = 2 * (len(contour) // 40) + 1
+        filtered_mbr_image = cv2.medianBlur(mbr_image, blur_core_size)
+
+        contours, _ = cv2.findContours(filtered_mbr_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        smoothed_contour = max(contours, key=cv2.contourArea)
+
+        # 计算曲率，即角度变化除以相邻点之间的距离
+        # 取样本80
+        sample_size = 80
+
+        # 计算步长，确保样本容量不会超过数组长度
+        step_size = max(1, len(smoothed_contour) // sample_size)
+        simpled_contour = smoothed_contour[::step_size]
+
+        #在图上标记点的序号
+        for index, point in enumerate(simpled_contour):
+            cv2.putText(self.src_img, str(index), (point[0][0], point[0][1]), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
+
+        cv2.drawContours(self.src_img, [smoothed_contour], -1, (0, 255, 0), 2)  # 第一部分用绿色绘制
+        cv2.imshow('Image with split contours', self.src_img)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+        curvatures = self.getCurvature(simpled_contour)
+        #curvatures = self.get_float_curvatures(smoothed_contour, 4)
 
         # 找到曲率绝对值最大两个负曲率对应的点
         #max_neg = 0
@@ -99,7 +119,7 @@ class Measure:
         if len(corners_index) < 2:
             #max_neg = 0
             max_pos = 0
-            for index in range(corners_index[0], len(curvatures)-1):
+            for index in range(corners_index[0] + 1, len(curvatures)-1):
                 val = curvatures[index]
                 if val > 0 and val > max_pos:
                     corners_index.append(index)
@@ -109,6 +129,7 @@ class Measure:
                 #     max_neg = val
 
         index_list = list(corners_index)
+        index_list = [item * step_size for item in index_list]
         # contour被分割成两个部分
         cv2.putText(img, 'o', (contour[index_list[0]][0][0], contour[index_list[0]][0][1]), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
                     (255, 0, 0), 1)
@@ -130,10 +151,9 @@ class Measure:
         cv2.waitKey(0)
         cv2.destroyAllWindows()
 
-    def getCurvature(self, contour):
+    def getCurvature(self, contour, gap=1):
         # 计算轮廓上的点的曲率
         curvatures = []
-        gap = 1
         for index in range(len(contour)):
             point = contour[index]
             x, y = point[0]
@@ -170,4 +190,67 @@ class Measure:
 
         return curvatures
 
-    #def sperate_cap_by_polygon(self):
+    def get_float_curvatures(self, contour, gap):
+        """
+        计算轮廓的浮点数曲率。 将像素坐标转换为浮点坐标
+        插值数量为 gap 轮廓总点数 变为len(contour) * (gap + 1)
+        参数:
+        contour - 轮廓的列表，表示一个封闭的曲线。
+        gap - 切点之间间隔点数。
+
+        返回值:
+        返回一个浮点数列表，包含轮廓上每个点的曲率。
+        """
+        contour_len = len(contour)
+        contour_float = contour.astype(np.float32)
+        # 获取轮廓点的x和y坐标
+        x = contour_float[:, 0, 0]
+        y = contour_float[:, 0, 1]
+
+        # 创建新的x和y坐标数组，用于存储插值后的点
+        new_x = np.empty((len(x) * (gap + 1)))
+        new_y = np.empty_like(new_x)
+
+        # 使用样条插值创建新的点
+        for i in range(len(x) - 1):
+            x_spline = np.linspace(x[i], x[i + 1], gap, endpoint=False)  # 创建等距参数值
+            spl = make_interp_spline(x, y)
+            y_spline = spl(x_spline)
+
+            new_x[i*gap] = x[i]
+            new_y[i*gap] = y[i]
+            new_x[i*gap+1: i*gap+gap+1] = x_spline
+            new_y[i*gap+1: i*gap+gap+1] = y_spline
+
+        # 处理最后一个点（如果需要循环回到起点）
+        if x[0] != x[-1] or y[0] != y[-1]:
+            x_spline = np.linspace(x[-1], x[0], gap, endpoint=False)  # 最后一个段不需要额外的点
+            y_spline = make_interp_spline(x, y)(x_spline)
+            new_x[-1*gap:] = x_spline
+            new_y[-1*gap:] = y_spline
+
+        # 将新的x和y坐标组合成新的轮廓点
+        interpolated_contour = np.column_stack((new_x, new_y)).astype(np.int32)
+
+        return self.getCurvature(interpolated_contour, gap)
+
+def mark_curvatures(self, contour, curvatures):
+        """
+        在轮廓上标记曲率值。
+
+        参数:
+        contour - 轮廓的列表，表示一个封闭的曲线。
+        curvatures - 一个浮点数列表，包含轮廓上每个点的曲率。
+
+        返回值:
+        无返回值，此函数主要用于在轮廓图上标注出各点的曲率值。
+        """
+        # 函数体实现标记轮廓上各点曲率值的逻辑
+        for index in range(len(curvatures)):
+            if index%10 ==0:
+                text = "{:.2f}".format(curvatures[index]*100)
+                cv2.putText(self.src_img, text, (contour[index][0][0], contour[index][0][1]), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
+                        (255, 0, 0), 1)
+        cv2.imshow('Image with split contours', self.src_img)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
